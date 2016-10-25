6985441563fb5467c64162bccefb34bd486c07f3
s20081428 <s20081428@gmail.com>
2016-10-26 02:43:17 +0800

    v1.1

--- myportal.py
+++ myportal.py
@@ -1,18 +1,20 @@
-from bs4 import BeautifulSoup
-import requests
 import datetime as dt
-from requests import session
 import getpass
 
+import requests
+from bs4 import BeautifulSoup
+from requests import session
 
 historys = []
 courses = []
 url = 'https://myportal.vtc.edu.hk/wps/myportal/!ut/p/a1/hY_NCoJAGACfpYPH9lv_yroJlQSSiYW5F_laN13bVslNfPzwAaLjwMAwwOAGTOMoazSy06hmZqvSPm72tpvQOLo6Bxra8S5I_cClJwoFFGz9S4gSHzJgwFoccSKDeI9KGCI1V59KkB5NU0r96KCwaDv0Fs3OCfUci3KFwxAaI3SFmgsyNealIP_XYt1WehdP5DVPIUc-T0D_xPsyDxdfp1LYiQ!!/'
 
 headers = {
-    'Cookie' : '',
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
+    'Cookie': '',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
 }
+
+
 def login(username, password):
     post_data = {
         'ajaxLogin': 'true',
@@ -26,32 +28,37 @@ def login(username, password):
         for cookie in request.cookies:
             headers['Cookie'] += str(cookie.name + "=" + cookie.value + ";")
 
-def get_loginURL():
-        login_pageurl ='https://myportal.vtc.edu.hk/wps/portal/'
-        wb_data = requests.get(login_pageurl)
-        soup = BeautifulSoup(wb_data.text,'lxml')
 
-        return str(soup.select('div > div > form')[0].get('action'))
+def get_loginURL():
+    login_pageurl = 'https://myportal.vtc.edu.hk/wps/portal/'
+    wb_data = requests.get(login_pageurl)
+    soup = BeautifulSoup(wb_data.text, 'lxml')
+
+    return str(soup.select('div > div > form')[0].get('action'))
+
 
 def calLateTime(arr_time, lesson_time, status):
     lesson_starttime = lesson_time[:5]
-    if  (status == 'Present'):
+    if (status == 'Present'):
         return ''
     elif (status == 'Late'):
-        return ((dt.datetime(int("2016"),int('12'),int('06'),int(arr_time[:2])
-                             ,int(arr_time[3:5])) - dt.datetime(int("2016"),int('12'),int('06')
-                             ,int(lesson_starttime[:2]),int(lesson_starttime[3:5]))).seconds)/60
+        return ((dt.datetime(int("2016"), int('12'), int('06'), int(arr_time[:2])
+                             , int(arr_time[3:5])) - dt.datetime(int("2016"), int('12'), int('06')
+                                                                 , int(lesson_starttime[:2]),
+                                                                 int(lesson_starttime[3:5]))).seconds) / 60
     elif (status == 'Absent'):
-        return  calAbsentTime(lesson_time)
+        return calAbsentTime(lesson_time)
+
 
 def calAbsentTime(lesson_time):
     lesson_StartTime = lesson_time[:5]
     lesson_EndTime = lesson_time[8:13]
 
+    return ((dt.datetime(int("2016"), int('12'), int('06'), int(lesson_EndTime[:2])
+                         , int(lesson_EndTime[3:5])) - dt.datetime(int("2016"), int('12'), int('06')
+                                                                   , int(lesson_StartTime[:2]),
+                                                                   int(lesson_StartTime[3:5]))).seconds) / 60
 
-    return ((dt.datetime(int("2016"),int('12'),int('06'),int(lesson_EndTime[:2])
-                            ,int(lesson_EndTime[3:5])) - dt.datetime(int("2016"),int('12'),int('06')
-                            ,int(lesson_StartTime[:2]),int(lesson_StartTime[3:5]))).seconds)/60
 
 def check_attend(SubjectCode):
     wb_data = requests.get(url, headers=headers)
@@ -67,8 +74,8 @@ def check_attend(SubjectCode):
 
     post_data = {
         'javax.faces.encodedURL': post_EncodedURL,
-        'viewns_Z7_1I9E13O0LGU2F0A1LD8Q583GO5_:j_id26950906_1_18bb2b55_SUBMIT':post_Submit,
-        'viewns_Z7_1I9E13O0LGU2F0A1LD8Q583GO5_:j_id26950906_1_18bb2b55:changeModuleButton':post_ChangeModuleButton,
+        'viewns_Z7_1I9E13O0LGU2F0A1LD8Q583GO5_:j_id26950906_1_18bb2b55_SUBMIT': post_Submit,
+        'viewns_Z7_1I9E13O0LGU2F0A1LD8Q583GO5_:j_id26950906_1_18bb2b55:changeModuleButton': post_ChangeModuleButton,
         'viewns_Z7_1I9E13O0LGU2F0A1LD8Q583GO5_:j_id26950906_1_18bb2b55:j_id26950906_1_18bb2b07': post_SubjectCode,
         'autoScroll': post_AutoScroll,
         'javax.faces.ViewState': post_ViewState
@@ -79,7 +86,7 @@ def check_attend(SubjectCode):
     soup2 = BeautifulSoup(wb_data2.text, 'lxml')
 
     isWrongCrouse = soup2.select('table.hkvtcsp_wording')
-    if len(isWrongCrouse)>0:
+    if len(isWrongCrouse) > 0:
         dates = soup2.select('tbody > tr > td:nth-of-type(1) ')
         statuss = soup2.select('tbody > tr > td:nth-of-type(2) ')
         arrive_times = soup2.select('tbody > tr > td:nth-of-type(3) ')
@@ -87,26 +94,28 @@ def check_attend(SubjectCode):
         class_rooms = soup2.select('tbody > tr > td:nth-of-type(5) ')
         subjectTitle = soup2.select('.hkvtcsp_textInput > option[selected="selected"]')[0].text
 
-        for date, status, arrive_time, class_time, class_room in zip(dates,statuss,arrive_times,class_times,class_rooms):
+        for date, status, arrive_time, class_time, class_room in zip(dates, statuss, arrive_times, class_times,
+                                                                     class_rooms):
             history1 = {
                 'date': date.get_text(),
                 'status': status.get_text(),
                 'arrive_time': arrive_time.get_text(),
                 'class_time': class_time.get_text(),
                 'class_rooms': class_room.get_text(),
-                'late_time': calLateTime(arrive_time.get_text(),class_time.get_text(),status.get_text())
+                'late_time': calLateTime(arrive_time.get_text(), class_time.get_text(), status.get_text())
             }
             historys.append(history1)
         cal_main(SubjectCode, subjectTitle)
     else:
         pass
 
-def cal_main (SubjectCode, subjectTitle):
+
+def cal_main(SubjectCode, subjectTitle):
     totalLate = 0
     global historys
     for history in historys:
-        if history['late_time']!="":
-            totalLate+= int(history['late_time'])
+        if history['late_time'] != "":
+            totalLate += int(history['late_time'])
 
     total_study_hours = {
         'ITP4506': 52,
@@ -123,11 +132,12 @@ def cal_main (SubjectCode, subjectTitle):
     if SubjectCode in total_study_hours:
         hours = total_study_hours[SubjectCode]
     else:
-        hours = input('Enter the total hour of course (' + SubjectCode  + ') : ')
-    print("Total left hours: " + str(round(float(totalLate)/60,2)))
-    print("Total left percentage " + str(round((((float(totalLate)/60)/float(hours))*100),2)) + "%")
-    print("You have " + str(round(float(hours)*0.3 - float(totalLate/60),2)) + " hours to leave.")
+        hours = input('Enter the total hour of course (' + SubjectCode + ') : ')
+    print("Total left hours: " + str(round(float(totalLate) / 60, 2)))
+    print("Total left percentage " + str(round((((float(totalLate) / 60) / float(hours)) * 100), 2)) + "%")
+    print("You have " + str(round(float(hours) * 0.3 - float(totalLate / 60), 2)) + " hours to leave.")
     historys = []
+
 
 def getSubjectcode():
     global courses
@@ -138,7 +148,8 @@ def getSubjectcode():
 
     for course in _courses:
         courses.append(course.get('value'))
-    courses.pop(0) #delete the first empty option
+    courses.pop(0)  # delete the first empty option
+
 
 def start():
     ask_Login()
@@ -149,10 +160,12 @@ def start():
         return
     ask_Input()
 
+
 def ask_Login():
     username = input("Username: ")
     password = getpass.getpass("Password: ")
-    login(username,password)
+    login(username, password)
+
 
 def ask_Input():
     check_type = input("1. Check all course /  2. Check assigned course [1,2]")
@@ -165,5 +178,6 @@ def ask_Input():
     else:
         print('Enter a voild input please!')
         ask_Input()
+
 
 start();
